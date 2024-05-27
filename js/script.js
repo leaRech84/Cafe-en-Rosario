216bdcec7006e0a549be7f9e9eb90335d8106866
leaRech84 <rechleandroluis1@gmail.com>
2024-05-27 04:05:04 -0300

    subiendo

--- js/script.js
+++ js/script.js
@@ -1,53 +1,30 @@
+const nombre = document.getElementById("name")
+const number = document.getElementById("telefono")
+const password = document.getElementById("name")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-
-let menuToggle = document.querySelector('.menuToggle');
-let header = document.querySelector('header');
-let section = document.querySelector('section');
-// let section = document.getElementsByTagName('*');
-
-
-menuToggle.onclick = function () {
-    header.classList.toggle('active')
-    section.classList.toggle('active')
-}
-
-const slides = document.querySelectorAll('.slide');
-
-console.log(slides)
-
-let counter = 0;
-console.log(i)
-
-
-
-slides.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`
-
-});
-
-const goPrev = () => {
-    counter--
-    slideImg()
-}
-const goNext = () => {
-    counter++ 
-    slideImg()
-}
-
-
-const slideImg = () => {
-    for (let i = 0; i < slides.length; i++) {
-        if (counter >= slides.length) {
-            slides.forEach(slide => {
-                slide.style.transform = `translateX(-${counter * 0}%)`
-            });
-        } else {
-            slides.forEach(slide => {
-                slide.style.transform = `translateX(-${counter * 100}%)`
-            });
-        }
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    if (nombre.value.length <6){
+        warnings += `El nombre no es válido <br>`
+        entrar = true
     }
-}
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
+
+
+
+
+
+
+
+
 // const slideImg = () => {
 //     for (let i = 0; i < slides.length; i++) {
 //         if (counter >= slides.length) {
